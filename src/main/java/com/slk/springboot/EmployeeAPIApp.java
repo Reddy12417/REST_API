87f4df75b64e283b5cb6a07c635df272a6981a89
srinivasreddii <57862240+srinivasreddii@users.noreply.github.com>
2020-01-01 17:46:06 +0530

    Update EmployeeAPIApp.java

--- src/main/java/com/slk/springboot/EmployeeAPIApp.java
+++ src/main/java/com/slk/springboot/EmployeeAPIApp.java
@@ -2,15 +2,16 @@ package com.slk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class EmployeeAPIApp extends SpringBootServletInitializer{
+public class EmployeeAPIApp extends org.springframework.boot.web.support.SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeAPIApp.class, args);
 	}
-    @Override
+	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(EmployeeAPIApp.class);
     }
